49f4a6271f4119f4198b78db5a910c2de0a34182
wilson <wilson_lyc@foxmail.com>
2026-03-01 17:39:15 +0800

    feat: 注释掉训练数据变换列表的打印信息，减少冗余输出

--- train.py
+++ train.py
@@ -157,7 +157,7 @@ if opt.erasing_p>0:
 if opt.color_jitter:
     transform_train_list = [transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0)] + transform_train_list
 
-print(transform_train_list)
+# print(transform_train_list)
 data_transforms = {
     'train': transforms.Compose( transform_train_list ),
     'val': transforms.Compose(transform_val_list),
