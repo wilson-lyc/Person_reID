67cd90f3fad49e851b09aa425511619ece4611f1
wilson <wilson_lyc@foxmail.com>
2026-03-01 16:53:48 +0800

    fix: 修复Swin模型初始化中的预训练权重路径

--- model.py
+++ model.py
@@ -153,8 +153,8 @@ class ft_net_swin(nn.Module):
 
     def __init__(self, class_num, droprate=0.5, stride=2, circle=False, linear_num=512):
         super(ft_net_swin, self).__init__()
-        # model_ft = timm.create_model('swin_base_patch4_window7_224', pretrained=True, drop_path_rate = 0.2)
-        model_ft = timm.create_model("hf-hub:timm/swin_base_patch4_window7_224.ms_in22k_ft_in1k", pretrained=True)
+        model_ft = timm.create_model('swin_base_patch4_window7_224', pretrained=True, drop_path_rate = 0.2)
+        # model_ft = timm.create_model("hf-hub:timm/swin_base_patch4_window7_224.ms_in22k_ft_in1k", pretrained=True)
         # avg pooling to global pooling
         #model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
         model_ft.head = nn.Sequential() # save memory
