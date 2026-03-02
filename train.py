167333bee441b5b5cc1f9bf7dffac96fce55ef47
wilson <wilson_lyc@foxmail.com>
2026-03-02 14:23:03 +0800

    fix(train): clean up code by removing commented sections and unnecessary imports

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from __future__ import print_function, division
 
 import argparse
@@ -12,7 +10,6 @@ import torch.backends.cudnn as cudnn
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-#from PIL import Image
 import time
 import os
 
@@ -29,23 +26,18 @@ from ODFA import ODFA
 from utils import save_network
 from tool.lark import lark_notify, lark_log
 version =  torch.__version__
-from pytorch_metric_learning import losses, miners #pip install pytorch-metric-learning
+from pytorch_metric_learning import losses, miners
 
-######################################################################
-# Options
-# --------
 parser = argparse.ArgumentParser(description='Training')
 parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
 parser.add_argument('--name',default='ft_ResNet50', type=str, help='output model name')
 parser.add_argument('--run_id', default='', type=str, help='external run id for logging')
-# data
 parser.add_argument('--data_dir',default='./data/Market/pytorch',type=str, help='training dir path')
 parser.add_argument('--train_all', action='store_true', help='use all training data' )
 parser.add_argument('--batchsize', default=32, type=int, help='batchsize')
 parser.add_argument('--color_jitter', action='store_true', help='use color jitter in training')
 parser.add_argument('--erasing_p', default=0, type=float, help='Random Erasing probability, in [0,1]')
 parser.add_argument('--DG', action='store_true', help='use extra DG-Market Dataset for training. Please download it from https://github.com/NVlabs/DG-Net#dg-market.' )
-# optimizer
 parser.add_argument('--lr', default=0.05, type=float, help='learning rate')
 parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay. More Regularization Smaller Weight.')
 parser.add_argument('--total_epoch', default=60, type=int, help='total training epoch')
@@ -54,7 +46,6 @@ parser.add_argument('--bf16', action='store_true', help='use bfloat16 instead of
 parser.add_argument('--cosine', action='store_true', help='use cosine lrRate' )
 parser.add_argument('--FSGD', action='store_true', help='use fused sgd, which will speed up trainig slightly. apex is needed.' )
 parser.add_argument('--wa', action='store_true', help='use weight average' )
-# backbone
 parser.add_argument('--linear_num', default=512, type=int, help='feature dimension: 512 or default or 0 (linear=False)')
 parser.add_argument('--stride', default=2, type=int, help='stride')
 parser.add_argument('--droprate', default=0.5, type=float, help='drop rate')
@@ -69,7 +60,6 @@ parser.add_argument('--use_convnext', action='store_true', help='use ConvNext')
 parser.add_argument('--ibn', action='store_true', help='use resnet+ibn')
 parser.add_argument('--usam', action='store_true', help='use resnet+usam (Joint Representation Learning and Keypoint Detection for Cross-view Geo-localization. TIP2022)')
 parser.add_argument('--PCB', action='store_true', help='use PCB+ResNet50')
-# loss
 parser.add_argument('--warm_epoch', default=0, type=int, help='the first K epoch that needs warm up')
 parser.add_argument('--arcface', action='store_true', help='use ArcFace loss' )
 parser.add_argument('--circle', action='store_true', help='use Circle loss' )
@@ -89,7 +79,7 @@ run_id = opt.run_id
 print(f"name={opt.name}")
 
 if opt.DG:
-    opt.wa = True #DG will enable swa.
+    opt.wa = True
 
 fp16 = opt.fp16
 bf16 = opt.bf16
@@ -107,15 +97,9 @@ for str_id in str_ids:
     if gid >=0:
         gpu_ids.append(gid)
 opt.gpu_ids = gpu_ids
-# set gpu ids
 if len(gpu_ids)>0:
-    #torch.cuda.set_device(gpu_ids[0])
     cudnn.enabled = True
     cudnn.benchmark = True
-######################################################################
-# Load Data
-# ---------
-#
 
 if opt.use_swin:
     h, w = 224, 224
@@ -123,7 +107,6 @@ else:
     h, w = 256, 128
 
 transform_train_list = [
-        #transforms.RandomResizedCrop(size=128, scale=(0.75,1.0), ratio=(0.75,1.3333), interpolation=3), #Image.BICUBIC)
         transforms.Resize((h, w), interpolation=3),
         transforms.Pad(10),
         transforms.RandomCrop((h, w)),
@@ -157,7 +140,6 @@ if opt.erasing_p>0:
 if opt.color_jitter:
     transform_train_list = [transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0)] + transform_train_list
 
-# print(transform_train_list)
 data_transforms = {
     'train': transforms.Compose( transform_train_list ),
     'val': transforms.Compose(transform_val_list),
@@ -178,7 +160,6 @@ dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.
                                              shuffle=True, num_workers=2, pin_memory=True, drop_last=True,
                                              prefetch_factor=2, persistent_workers=True) # 8 workers may work faster
               for x in ['train', 'val']}
-# Use extra DG-Market Dataset for training. Please download it from https://github.com/NVlabs/DG-Net#dg-market.
 if opt.DG:
     if not os.path.isdir('../DG-Market'):
         os.system('gdown 126Gn90Tzpk3zWp2c7OBYPKc-ZjhptKDo')
@@ -198,21 +179,7 @@ use_gpu = torch.cuda.is_available()
 
 since = time.time()
 inputs, classes = next(iter(dataloaders['train']))
-# print(time.time()-since)
-######################################################################
-# Training the model
-# ------------------
-#
-# Now, let's write a general function to train a model. Here, we will
-# illustrate:
-#
-# -  Scheduling the learning rate
-# -  Saving the best model
-#
-# In the following, parameter ``scheduler`` is an LR scheduler object from
-# ``torch.optim.lr_scheduler``.
-
-y_loss = {} # loss history
+y_loss = {}
 y_loss['train'] = []
 y_loss['val'] = []
 y_err = {}
@@ -234,7 +201,7 @@ def format_epoch_tag(epoch_idx, total_epochs):
     return f"Epoch {epoch_idx + 1:0{width}d}/{total_epochs:0{width}d}"
 
 def fliplr(img):
-    '''flip horizontal'''
+    """Horizontal flip on width dimension."""
     inv_idx = torch.arange(img.size(3)-1,-1,-1).long().cuda()  # N x C x H x W
     img_flip = img.index_select(3,inv_idx)
     return img_flip
@@ -243,11 +210,9 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
     since = time.time()
     best_val_acc = 0.0
 
-    #best_model_wts = model.state_dict()
-    #best_acc = 0.0
     wa_flag = opt.wa
-    warm_up = 0.1 # We start from the 0.1*lrRate
-    warm_iteration = round(dataset_sizes['train']/opt.batchsize)*opt.warm_epoch # first 5 epoch
+    warm_up = 0.1
+    warm_iteration = round(dataset_sizes['train']/opt.batchsize)*opt.warm_epoch
     if opt.PCB:
         embedding_size = model.classifier0.linear_num
     else:
@@ -342,17 +307,15 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
             swa_model.avg_fn = swa_utils.get_ema_avg_fn(decay=0.996)
             print("start weight avg")
         
-        # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
-                model.train(True)  # Set model to training mode
+                model.train(True)
             else:
-                model.train(False)  # Set model to evaluate mode
-
-            # Keep batch feedback in tqdm; use compact epoch summary after each epoch.
+                model.train(False)
+                
             pbar = tqdm(
                 total=len(dataloaders[phase]),
-                desc=f"[{phase}]",
+                desc=f"{phase}",
                 leave=True,
                 dynamic_ncols=True,
                 bar_format="{desc:<10} {percentage:3.0f}%|{bar:24}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]",
@@ -362,27 +325,17 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
 
             running_loss = 0.0
             running_corrects = 0.0
-            # Iterate over data.
             for iter, data in enumerate(dataloaders[phase]):
-                # get the inputs
                 inputs, labels = data
                 now_batch_size,c,h,w = inputs.shape
                 pbar.update(1)
                 if now_batch_size<opt.batchsize: # skip the last batch
                     continue
-                #print(inputs.shape)
-                # wrap them in Variable
                 if use_gpu:
                     inputs = inputs.cuda().detach()
                     labels = labels.cuda().detach()
-                # if we use low precision, input also need to be fp16
-                #if fp16:
-                #    inputs = inputs.half()
-
-                # zero the parameter gradients
                 optimizer.zero_grad()
 
-                # forward
                 if phase == 'val':
                     with torch.no_grad():
                         outputs = model(inputs)
@@ -446,9 +399,7 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
                         loss += opt.adv * criterion(outputs_adv, labels)
 
                 del inputs
-                # use extra DG Dataset (https://github.com/NVlabs/DG-Net#dg-market)
                 if opt.DG and phase == 'train' and epoch > num_epochs*0.8:
-                    # print("DG-Market is involved. It will double the training time.")
                     try:
                         _, batch = DGloader_iter.__next__()
                     except StopIteration: 
@@ -461,7 +412,6 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
                     inputs1, inputs2, _ = batch
                     inputs1 = inputs1.cuda().detach()
                     inputs2 = inputs2.cuda().detach()
-                    # use memory in vivo loss (https://arxiv.org/abs/1912.11164)
                     if bf16 or fp16:
                         with torch.amp.autocast(device_type='cuda', dtype=dtype16):
                             outputs1 = model(inputs1)
@@ -477,7 +427,7 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
 
                     swa_model.eval()
                     with torch.no_grad():
-                        outputs2 = swa_model(inputs2) #stop gradient like dino
+                        outputs2 = swa_model(inputs2)
 
                     if return_feature:
                         outputs2, _ = outputs2
@@ -486,43 +436,36 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
                             part[i] = outputs2[i]
                         outputs2 = (sm(part[0]) + sm(part[1]) +sm(part[2]) + sm(part[3]) +sm(part[4]) +sm(part[5]))/6
 
-                    #supervised via teacher like dino. previous use sm(outputs1 + outputs2)
                     kl_loss = nn.KLDivLoss(reduction='batchmean')
                     reg= (kl_loss(log_sm(outputs2), sm(outputs1))  + kl_loss(log_sm(outputs1) , sm(outputs2)))/2
                     loss += 0.1*reg
                     del inputs1, inputs2
-                    #print(0.01*reg)
-                # backward + optimize only if in training phase
                 if epoch<opt.warm_epoch and phase == 'train': 
                     warm_up = min(1.0, warm_up + 0.9 / warm_iteration)
                     loss = loss*warm_up
 
                 if phase == 'train':
-                    if bf16 or fp16: # we use optimier to backward loss
+                    if bf16 or fp16:
                         scaler.scale(loss).backward()
-                        scaler.step(optimizer) # a safety optimizer.step()
+                        scaler.step(optimizer)
                         scaler.update()
                     else:
                         loss.backward()
                         optimizer.step()
-                # statistics
-                if int(version[0])>0 or int(version[2]) > 3: # for the new version like 0.4.0, 0.5.0 and 1.0.0
+                if int(version[0])>0 or int(version[2]) > 3:
                     running_loss += loss.item() * now_batch_size
                     batch_loss = float(loss.item())
-                else :  # for the old version like 0.3.0 and 0.3.1
+                else :
                     running_loss += loss.data[0] * now_batch_size
                     batch_loss = float(loss.data[0])
                 del loss
                 running_corrects += float(torch.sum(preds == labels.data))
-                # Refresh the progress bar in every batch
                 batch_acc = float(torch.sum(preds == labels.data)) / now_batch_size
                 pbar.set_postfix_str(f"loss={batch_loss:.4f}, acc={batch_acc:.4f}")
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects / dataset_sizes[phase]
             
-            # print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-            #     phase, epoch_loss, epoch_acc))
             pbar.set_postfix_str(f"loss={epoch_loss:.4f}, acc={epoch_acc:.4f}")
             pbar.close()
             
@@ -533,7 +476,6 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
             y_loss[phase].append(epoch_loss)
             y_err[phase].append(1.0-epoch_acc)            
             epoch_stats[phase] = {"loss": epoch_loss, "acc": epoch_acc}
-            # deep copy the model
             if phase == 'val' and epoch%10 == 9:
                 last_model_wts = model.state_dict()
                 if len(opt.gpu_ids)>1:
@@ -590,9 +532,6 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
 
     time_elapsed = time.time() - since
     print(f"training complete in {format_duration(time_elapsed)}")
-    #print('Best val Acc: {:4f}'.format(best_acc)
-
-    # load best model weights
     model.load_state_dict(last_model_wts)
     if len(opt.gpu_ids)>1:
         save_network(model.module, opt.name, 'last')
@@ -634,10 +573,6 @@ def train_model(model, criterion, optimizer, scheduler, scaler, num_epochs=25):
 
     return model
 
-
-######################################################################
-# Draw Curve
-#---------------------------
 x_epoch = []
 fig = plt.figure()
 ax0 = fig.add_subplot(121, title="loss")
@@ -652,14 +587,6 @@ def draw_curve(current_epoch):
         ax0.legend()
         ax1.legend()
     fig.savefig( os.path.join('./model',name,'train.jpg'))
-
-
-######################################################################
-# Finetuning the convnet
-# ----------------------
-#
-# Load a pretrainied model and reset final fully connected layer.
-#
 
 return_feature = opt.arcface or opt.cosface or opt.circle or opt.triplet or opt.contrast or opt.instance or opt.lifted or opt.sphere
 
@@ -686,19 +613,11 @@ if opt.PCB:
     model = PCB(len(class_names))
 
 opt.nclasses = len(class_names)
-# print(model)
-# model to gpu
 model = model.cuda()
 
-optim_name = optim.SGD #apex.optimizers.FusedSGD
-if opt.FSGD: # apex is needed
+optim_name = optim.SGD
+if opt.FSGD:
     optim_name = FusedSGD
-
-#if torch.cuda.get_device_capability()[0]>6 and len(opt.gpu_ids)==1 and int(version[0])>1: # should be >=7 and one gpu
-    #torch.set_float32_matmul_precision('high')
-    #print("Compiling model... The first epoch may be slow, which is expected!")
-    # https://huggingface.co/docs/diffusers/main/en/optimization/torch2.0
-    #model = torch.compile(model, mode="reduce-overhead", dynamic = True) # pytorch 2.0
 
 if len(opt.gpu_ids)>1:
     model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids) 
@@ -718,8 +637,6 @@ if len(opt.gpu_ids)>1:
                      +list(map(id, model.module.classifier3.parameters() ))
                      +list(map(id, model.module.classifier4.parameters() ))
                      +list(map(id, model.module.classifier5.parameters() ))
-                     #+list(map(id, model.module.classifier6.parameters() ))
-                     #+list(map(id, model.module.classifier7.parameters() ))
                       )
         base_params = filter(lambda p: id(p) not in ignored_params, model.module.parameters())
         classifier_params = filter(lambda p: id(p) in ignored_params, model.module.parameters())
@@ -744,8 +661,6 @@ else:
                      +list(map(id, model.classifier3.parameters() ))
                      +list(map(id, model.classifier4.parameters() ))
                      +list(map(id, model.classifier5.parameters() ))
-                     #+list(map(id, model.classifier6.parameters() ))
-                     #+list(map(id, model.classifier7.parameters() ))
                       )
         base_params = filter(lambda p: id(p) not in ignored_params, model.parameters())
         classifier_params = filter(lambda p: id(p) in ignored_params, model.parameters())
@@ -754,25 +669,16 @@ else:
              {'params': classifier_params, 'lr': opt.lr}
          ], weight_decay=opt.weight_decay, momentum=0.9, nesterov=True)
 
-# Decay LR by a factor of 0.1 every 40 epochs
 exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=opt.total_epoch*2//3, gamma=0.1)
 if opt.cosine:
     exp_lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer_ft, opt.total_epoch, eta_min=0.01*opt.lr)
 
-######################################################################
-# Train and evaluate
-# ^^^^^^^^^^^^^^^^^^
-#
-# It should take around 1-2 hours on GPU. 
-#
 dir_name = os.path.join('./model',name)
 if not os.path.isdir(dir_name):
     os.mkdir(dir_name)
-#record every run
 copyfile('./train.py', dir_name+'/train.py')
 copyfile('./model.py', dir_name+'/model.py')
 
-# save opts
 with open('%s/opts.yaml'%dir_name,'w') as fp:
     yaml.dump(vars(opt), fp, default_flow_style=False)
 
